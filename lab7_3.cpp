56bd0e184c387084bcfef8c61469717c864fbf80
nithikon4410 <nithikon_jansanitsri@cmu.ac.th>
2020-12-29 16:19:52 +0700

    lab7_3.cpp

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,11 +1,50 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your property: "
-"Your status = "
-"UNFRIEND"
-"FRIEND"
-"BEST FRIEND"
-"ONE-NIGHT-STAND"
-"MARRIED"
-*/
+#include<iostream>
+#include<iomanip>
+#include<string>
+using namespace std;
+
+char findGrade(double x){
+    if(x >90)
+        return 'A';
+    else if (x>75)
+        return 'B';
+    else if(x>60)
+        return 'C';
+    else if(x>45)
+        return 'D';
+    else
+        return 'F';
+    
+}
+
+int main(){
+	int N,i = 0;
+	cout << "Enter the number of students: ";
+	cin >> N;
+	string name[N];
+	double score[N];	
+	
+	
+	while(i < N){
+		cout << "Name of student " << i+1 << ": ";
+		getline(cin,name[i]);
+		cin.ignore();
+		cout << "Score of student " << i+1 << ": ";
+		cin >> score[i];
+		cin.ignore();
+		i++;
+	}
+	
+	
+	i = 0;
+	cout << "---------------------------------------------\n";
+	cout << setw(25) << "Name" << setw(8) << "Score" << setw(8) << "Grade" << "\n";
+	cout << "---------------------------------------------\n";
+	while(i < N){
+		cout << setw(25) << name[i] << setw(8) << score[i] << setw(8) << findGrade(score[i]) << "\n";
+		i++;
+	} 
+	cout << "---------------------------------------------\n";
+
+	return 0;
+}
